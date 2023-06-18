b625f10a2cd3936e7fd9558e6260f7cead4185bd
RZorkin <zorkin81@gmail.com>
2023-06-18 13:48:45 +0300

    Add project files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from '../redux/contactsOperations';
+
+import { useSelector } from 'react-redux';
+import { selectorContscts } from '../redux/selectors';
+import { selectorIsLoading } from '../redux/selectors';
+import { selectorError } from '../redux/selectors';
+
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { Section } from './Section/Section';
+
 export const App = () => {
+  const contacts = useSelector(selectorContscts);
+  const error = useSelector(selectorError);
+  const isLoading = useSelector(selectorIsLoading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Phonebook">
+        <ContactForm />
+      </Section>
+      <Section title="Contacts">
+        <Filter />
+        {isLoading && !error && <b>Request in progress...</b>}
+        {error && error}
+        <ContactList />
+        {contacts.length > 0 && <ContactList />}
+      </Section>
+    </>
   );
 };
